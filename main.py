ca7b77c88364470be361bfcb39c9b720e66e36df
Mosaad Ahmed Mohammed <mosaadahmed@Mosaads-MacBook-Air.local>
2026-07-28 17:25:14 -0400

    made compariso by key opsional

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ class AnalyzeRequest(BaseModel):
 class CompareRequest(BaseModel):
     table_a: str
     table_b: str
-    key_column: str = None
+    key_column: Optional[str] = None
 
 
 @app.get("/")
